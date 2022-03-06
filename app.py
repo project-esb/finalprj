c0f4ea4d91643d71917647bc488854bfe811a036
project-esb <85157731+project-esb@users.noreply.github.com>
2022-03-06 18:01:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 import streamlit as st
 import hydralit_components as hc
+import datetime
 
-#can apply customisation to almost all the properties of the card, including the progress bar
-theme_bad = {'bgcolor': '#FFF0F0','title_color': 'red','content_color': 'red','icon_color': 'red', 'icon': 'fa fa-times-circle'}
-theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
-theme_good = {'bgcolor': '#EFF8F7','title_color': 'green','content_color': 'green','icon_color': 'green', 'icon': 'fa fa-check-circle'}
+#make it look nice from the start
+st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
 
-cc = st.columns(4)
+# specify the primary menu definition
+menu_data = [
+    {'icon': "far fa-copy", 'label':"Left End"},
+    {'id':'Copy','icon':"🐙",'label':"Copy"},
+    {'icon': "fa-solid fa-radar",'label':"Dropdown1", 'submenu':[{'id':' subid11','icon': "fa fa-paperclip", 'label':"Sub-item 1"},{'id':'subid12','icon': "💀", 'label':"Sub-item 2"},{'id':'subid13','icon': "fa fa-database", 'label':"Sub-item 3"}]},
+    {'icon': "far fa-chart-bar", 'label':"Chart"},#no tooltip message
+    {'id':' Crazy return value 💀','icon': "💀", 'label':"Calendar"},
+    {'icon': "fas fa-tachometer-alt", 'label':"Dashboard",'ttip':"I'm the Dashboard tooltip!"}, #can add a tooltip message
+    {'icon': "far fa-copy", 'label':"Right End"},
+    {'icon': "fa-solid fa-radar",'label':"Dropdown2", 'submenu':[{'label':"Sub-item 1", 'icon': "fa fa-meh"},{'label':"Sub-item 2"},{'icon':'🙉','label':"Sub-item 3",}]},
+]
 
-with cc[0]:
- # can just use 'good', 'bad', 'neutral' sentiment to auto color the card
- hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good',bar_value=77)
+over_theme = {'txc_inactive': '#FFFFFF'}
+menu_id = hc.nav_bar(
+    menu_definition=menu_data,
+    override_theme=over_theme,
+    home_name='Home',
+    login_name='Logout',
+    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
+    sticky_nav=True, #at the top or not
+    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+)
 
-with cc[1]:
- hc.info_card(title='Some BAD BAD', content='This is really bad',bar_value=12,theme_override=theme_bad)
+if st.button('click me'):
+  st.info('You clicked at: {}'.format(datetime.datetime.now()))
 
-with cc[2]:
- hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral',bar_value=55)
 
-with cc[3]:
- #customise the the theming for a neutral content
- hc.info_card(title='Some NEURAL',content='Maybe...',key='sec',bar_value=5,theme_override=theme_neutral)
+if st.sidebar.button('click me too'):
+  st.info('You clicked at: {}'.format(datetime.datetime.now()))
 
+#get the id of the menu item clicked
+st.info(f"{menu_id}")
