e18d1139ee5354bdfb54b2dd9cdef466f4a8d2e2
project-esb <85157731+project-esb@users.noreply.github.com>
2022-03-06 18:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-from flask import Flask
-from flask.templating import render_template
+import streamlit as st
 
-app = Flask(__name__, static_url_path='/static')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', name='home')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
